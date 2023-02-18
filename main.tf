67fa6120955f34a13c5b819d3d7dd3ec82c37bd3
zyzyx <fauzuwan.nazri93@gmail.com>
2023-02-18 20:49:46 +0800

    add local provisioner

--- main.tf
+++ main.tf
@@ -74,15 +74,23 @@ resource "aws_instance" "dev_node" {
   key_name               = aws_key_pair.mtc_auth.id
   vpc_security_group_ids = [aws_security_group.mtc_sg.id]
   subnet_id              = aws_subnet.mtc_public_subnet.id
-  user_data = file("userdata.tpl")
+  user_data              = file("userdata.tpl")
 
+  root_block_device {
+    volume_size = 10
+  }
 
   tags = {
     Name = "dev_node"
   }
 
-
-  root_block_device {
-    volume_size = 10
+  provisioner "local-exec" {
+    command = templatefile("linux-ssh-config.tpl", {
+      hostname     = self.public_ip,
+      user         = "ubuntu",
+      identityfile = "~/.ssh/mtckey"
+    })
+    # interpreter = ["Powershell", "Command"]
+    interpreter = ["bash", "-c"]
   }
 }
