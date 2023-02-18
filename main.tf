ceccbed1ce34e8cbd5e4775b536c2c0a7b7adb53
zyzyx <fauzuwan.nazri93@gmail.com>
2023-02-18 19:05:25 +0800

    add aws ssh keypair

--- main.tf
+++ main.tf
@@ -56,9 +56,14 @@ resource "aws_security_group" "mtc_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port  = 0
-    to_port    = 0
-    protocol   = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_key_pair" "mtc_auth" {
+  key_name   = "mtckey"
+  public_key = file("~/.ssh/mtckey.pub")
 }
